bd3bec4168a3b06150afc6066fc77fbd4d5f401e
Roberto <roberto@morales.com.co>
2018-04-21 16:37:21 -0500

    Desarrollo faltante personalizacion recycler

---
+++ app/src/main/java/com/example/android/personasmaterialdiplomado20181/Principal.java
@@ -0,0 +1,35 @@
+package com.example.android.personasmaterialdiplomado20181;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Principal extends AppCompatActivity {
+    private RecyclerView lstOpciones;
+    private Intent i;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               i = new Intent(Principal.this,AgregarPersona.class);
+               startActivity(i);
+            }
+        });
+    }
+
+
+}
